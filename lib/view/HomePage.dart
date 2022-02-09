ab8b1a355eec96a0165b2efed75a4feda32c7ef5
someng2 <bsm4045@naver.com>
2022-02-09 16:15:10 +0900

    someng - 기본 화면

--- lib/view/HomePage.dart
+++ lib/view/HomePage.dart
@@ -1,1 +1,34 @@
+// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:voskat/view/SimulationPage.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('메인 페이지'),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              TextButton(
+                child: Text('모의훈련 페이지'),
+                onPressed: () {
+                  Get.to(SimulationPage());
+                },
+              )
+            ],
+          ),
+        ));
+  }
+}
