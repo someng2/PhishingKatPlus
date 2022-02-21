8bd9a921ad8f2327810d9f32669593542f61c379
someng2 <bsm4045@naver.com>
2022-02-21 18:08:18 +0900

    한글 키보드 잘림 현상 버그 발견ㅠ

--- lib/customWidget/A1/A1a_app.dart
+++ lib/customWidget/A1/A1a_app.dart
@@ -41,113 +41,121 @@ class _A1a_appPageState extends State<A1a_appPage> {
     String info2 = (widget.subtype == '대출사기') ? '주민등록번호' : '';
 
     final info1Controller = TextEditingController();
-    final info2Controller = TextEditingController();
-    final info3Controller = TextEditingController();
+    final info2_1Controller = TextEditingController();
+    final info2_2Controller = TextEditingController();
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text(appName),
-          // leading: Container(),
-          // actions: [
-          //   IconButton(
-          //     icon: Icon(Icons.close),
-          //     onPressed: () {
-          //       bool isFilled = checkInfo(
-          //           info1Controller, info2Controller, info3Controller);
-          //     },
-          //   )
-          // ],
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: Text(appName),
+        // leading: Container(),
+        // actions: [
+        //   IconButton(
+        //     icon: Icon(Icons.close),
+        //     onPressed: () {
+        //       bool isFilled = checkInfo(
+        //           info1Controller, info2Controller, info3Controller);
+        //     },
+        //   )
+        // ],
+      ),
+      body:
+          Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        Container(
+          padding: EdgeInsets.only(top: 100.h),
+          child: Row(children: [
+            Container(
+              width: 110.w,
+              padding: EdgeInsets.only(left: 20.h),
+              child: Text(info1,
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600)),
+            ),
+            Container(
+              width: 180.w,
+              height: 40.h,
+              padding: EdgeInsets.only(left: 20.w),
+              child: TextField(
+                  controller: info1Controller,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    contentPadding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                  ),
+                  textAlignVertical: TextAlignVertical.center,
+                  style: TextStyle(fontSize: 17.w)),
+            )
+          ]),
         ),
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                padding: EdgeInsets.only(top: 100.h),
-                child: Row(children: [
-                  Container(
-                    width: 110.w,
-                    padding: EdgeInsets.only(left: 20.h),
-                    child: Text(info1,
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.w600)),
+        Container(
+          padding: EdgeInsets.only(top: 30.h),
+          child: Row(children: [
+            Container(
+              width: 110.w,
+              padding: EdgeInsets.only(left: 20.w),
+              child: Text(info2,
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600)),
+            ),
+            Container(
+              width: 120.w,
+              height: 40.h,
+              padding: EdgeInsets.only(left: 20),
+              child: TextField(
+                  controller: info2_1Controller,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    contentPadding: EdgeInsets.fromLTRB(10, 0, 0, 0),
                   ),
-                  Container(
-                    width: 180.w,
-                    height: 40.h,
-                    padding: EdgeInsets.only(left: 20.w),
-                    child: TextField(
-                        controller: info1Controller,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                        ),
-                        style: TextStyle(fontSize: 17.w)),
-                  )
-                ]),
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 30.h),
-                child: Row(children: [
-                  Container(
-                    width: 110.w,
-                    padding: EdgeInsets.only(left: 20.w),
-                    child: Text(info2,
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.w600)),
+                  style: TextStyle(fontSize: 16, )),
+            ),
+            Container(child: Text(' - ', style: TextStyle(fontSize: 16))),
+            Container(
+              width: 100.w,
+              height: 40.h,
+              // padding: EdgeInsets.only(left: 20),
+              child: TextField(
+                obscureText: true,
+                  controller: info2_2Controller,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    contentPadding: EdgeInsets.fromLTRB(10, 0, 0, 0),
                   ),
-                  Container(
-                    width: 120.w,
-                    height: 40.h,
-                    padding: EdgeInsets.only(left: 20),
-                    child: TextField(
-                        controller: info2Controller,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                        ),
-                        style: TextStyle(fontSize: 16)),
-                  ),
-                  Container(child: Text(' - ', style: TextStyle(fontSize: 16))),
-                  Container(
-                    width: 100.w,
-                    height: 40.h,
-                    // padding: EdgeInsets.only(left: 20),
-                    child: TextField(
-                        controller: info3Controller,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                        ),
-                        style: TextStyle(fontSize: 16)),
-                  )
-                ]),
-              ),
-              SizedBox(height: 40.h),
+                  style: TextStyle(fontSize: 16)),
+            )
+          ]),
+        ),
+        SizedBox(height: 40.h),
 
-              // U2-a
-              Container(
-                  width: 120,
-                  child: ElevatedButton(
-                    child: Text('조회하기'),
-                    onPressed: () {
-                      bool isFilled = checkInfo(
-                          info1Controller, info2Controller, info3Controller);
-                      if (!isFilled) {
-                        Get.showSnackbar(
-                          GetSnackBar(
-                            title: '조회 불가!',
-                            message: '모든 정보를 입력하세요.',
-                            duration: Duration(seconds: 2),
-                            snackPosition: SnackPosition.BOTTOM,
-                          ),
-                        );
-                      } else {
-                        Get.to(A2aPage(
-                            subtype: widget.subtype, appName: widget.appName));
-                      }
-                    },
-                  )),
-              SizedBox(height: 200.h),
-              _showAd ? vaccineApp() : Container(height: 90.h)
-              // vaccineApp()
-            ]));
+        // U2-a
+        Container(
+            width: 120,
+            child: ElevatedButton(
+              child: Text('조회하기'),
+              onPressed: () {
+                bool isFilled = checkInfo(
+                    info1Controller, info2_1Controller, info2_2Controller);
+                if (!isFilled) {
+                  Get.showSnackbar(
+                    GetSnackBar(
+                      title: '조회 불가!',
+                      message: '모든 정보를 입력하세요.',
+                      duration: Duration(seconds: 2),
+                      snackPosition: SnackPosition.BOTTOM,
+                    ),
+                  );
+                } else {
+                  Get.to(A2aPage(
+                      subtype: widget.subtype,
+                      appName: widget.appName,
+                      info1: info1Controller.text,
+                      info2_1: info2_1Controller.text,
+                      info2_2: info2_2Controller.text));
+                }
+              },
+            )),
+        SizedBox(height: 200.h),
+        _showAd ? vaccineApp() : Container(height: 90.h)
+        // vaccineApp()
+      ]),
+    );
   }
 
   checkInfo(
--- lib/customWidget/A2/A2a.dart
+++ lib/customWidget/A2/A2a.dart
@@ -1,19 +1,50 @@
 // ignore_for_file: file_names
 
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class A2aPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:voskat/customWidget/vaccineApp.dart';
+
+class A2aPage extends StatefulWidget {
   String subtype;
   String appName;
-  A2aPage({Key? key, required this.subtype, required this.appName})
+  String info1;
+  String info2_1;
+  String info2_2;
+  A2aPage(
+      {Key? key,
+      required this.subtype,
+      required this.appName,
+      required this.info1,
+      required this.info2_1,
+      required this.info2_2})
       : super(key: key);
+
+  @override
+  State<A2aPage> createState() => _A2aPageState();
+}
+
+class _A2aPageState extends State<A2aPage> {
+
+  bool _showAd = false;
+
+  @override
+  void initState() {
+    Timer(Duration(seconds: 3), () {
+      setState(() {
+        _showAd = true;
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(appName),
-        leading: Container(),
+        title: Text(widget.appName),
+        // leading: Container(),
         actions: [
           // U3-a
           IconButton(
@@ -23,26 +54,40 @@ class A2aPage extends StatelessWidget {
         ],
       ),
       body: Container(
-        padding: EdgeInsets.only(top: 30, left: 30, right: 30),
+        padding: EdgeInsets.only(top: 30.h),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Container(
-                child: Row(children: [
-              Text(
-                '정보 조회 결과',
-                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
-              )
-            ])),
-            SizedBox(height:40),
-            Container(
-              height: 200,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  width: 1,
-                  color: Colors.grey
-                ),
+              padding: EdgeInsets.only(left: 20.w, right: 20.w),
+              child: Column(
+                children: [
+                  Container(
+                      child: Row(children: [
+                    if (widget.subtype == '대출사기')
+                      Text(
+                        '${widget.info1} 님의 ',
+                        style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
+                      ),
+                    Text(
+                      '정보 조회 결과',
+                      style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
+                    )
+                  ])),
+                  SizedBox(height: 30.h),
+                  Container(
+                    height: 200,
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 1, color: Colors.grey),
+                    ),
+                  ),
+                ],
               ),
-            )
+            ),
+            // SizedBox(height: 40),
+
+            // SizedBox(height: 200.h),
+            _showAd ? vaccineApp() : Container(height: 90.h)
           ],
         ),
       ),
