71350bcd64a596574cca6390f55c32f9e7bd3b9e
someng2 <bsm4045@naver.com>
2022-04-28 00:49:17 +0900

    someng - 대출사기 신고하기 flow 구현 완료

--- lib/view/customWidget/customDialog.dart
+++ lib/view/customWidget/customDialog.dart
@@ -10,9 +10,12 @@ import 'package:voskat/view/customWidget/A3/A3bPage.dart';
 import 'package:voskat/model/simulation/userAction.dart';
 import 'package:voskat/controller/ScenarioController.dart';
 
+import 'A3/A3cPage.dart';
+
 class CustomDialog extends StatelessWidget {
   final String sid;
   final String aid;
+  final double height;
   UserAction userCancelAction;
   UserAction userOkAction;
 
@@ -20,6 +23,7 @@ class CustomDialog extends StatelessWidget {
       {Key? key,
       required this.sid,
       required this.aid,
+      required this.height,
       required this.userOkAction,
       required this.userCancelAction})
       : super(key: key);
@@ -38,19 +42,26 @@ class CustomDialog extends StatelessWidget {
 
     if (aid == 'A2-c') {
       title = '신고하기';
+      description = '이 번호의 전화 및 메시지를 더이상 받지 않습니다.\n';
+
+      // 이 번호를 한국인터넷 진흥원(118)에 신고하시겠습니까? ';
+    }
+
+    if (aid == 'A2-d') {
+      title = '신고하기';
       description = '이 번호를 한국인터넷 진흥원(118)에 신고하시겠습니까?';
     }
 
     return SingleChildScrollView(
       child: Padding(
-        padding: EdgeInsets.only(top: 537.h),
+        padding: EdgeInsets.only(top: 681 - height),
         child: Dialog(
           // insetPadding: EdgeInsets.only(bottom: 300.h,),
           shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.all(Radius.circular(20))),
           child: Container(
               width: 251,
-              height: 144,
+              height: height,
               padding: EdgeInsets.only(
                 top: 19,
                 left: 21,
@@ -59,44 +70,94 @@ class CustomDialog extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Container(
-                    height: 25.h,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  if (aid != 'A2-c')
+                    Column(
                       children: [
-                        Text(
-                          title,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 15,
+                        Container(
+                          height: 25,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                title,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  if (aid == 'A2-d')
+                                    Container(
+                                      width: 70,
+                                      height: 25,
+                                      child: Image.asset('image/kisaLogo.png',
+                                          fit: BoxFit.fitWidth),
+                                      // decoration: BoxDecoration(border: Border.all()),
+                                    ),
+                                  SizedBox(width: 4),
+                                ],
+                              )
+                            ],
                           ),
                         ),
-                        Row(
-                          children: [
-                            if (aid == 'A2-c')
-                              Container(
-                              width: 70.w,
-                              height: 25.h,
-                              child: Image.asset('image/kisaLogo.png', fit: BoxFit.fitWidth),
-                              // decoration: BoxDecoration(border: Border.all()),
-                            ),
-                            SizedBox(width: 4.w),
-                          ],
-                        )
+                        SizedBox(height: 13),
                       ],
                     ),
-                  ),
-                  SizedBox(height: 13),
-                  Container(
-                    width: 209.w,
-                    child: Text(
-                      description,
-                      style: TextStyle(
-                        fontSize: 13,
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 13),
+                  (aid == 'A2-c')
+                      ? Container(
+                          width: 209,
+                          padding: EdgeInsets.only(bottom: 17),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                description,
+                                style: TextStyle(
+                                  fontSize: 13,
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    '이 번호를 ',
+                                    style: TextStyle(
+                                      fontSize: 13,
+                                    ),
+                                  ),
+
+                                  // TODO: bold font 적용
+                                  Text('한국인터넷 진흥원(118)',
+                                      style: TextStyle(
+                                        fontSize: 13,
+                                        color: Color(0xff0473e1),
+                                      )),
+                                  Text(
+                                    '에',
+                                    style: TextStyle(
+                                      fontSize: 13,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Text(
+                                '신고하시겠습니까?',
+                                style: TextStyle(
+                                  fontSize: 13,
+                                ),
+                              ),
+                            ],
+                          ))
+                      : Container(
+                          width: 209,
+                          padding: EdgeInsets.only(bottom: 13),
+                          child: Text(
+                            description,
+                            style: TextStyle(
+                              fontSize: 13,
+                            ),
+                          ),
+                        ),
                   Container(
                       child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -105,31 +166,50 @@ class CustomDialog extends StatelessWidget {
                           width: 89,
                           height: 26,
                           decoration: BoxDecoration(
-                              color: Colors.grey,
-                              borderRadius: BorderRadius.circular(13)),
+                              color: Color(0xffe7e7e7),
+                              borderRadius: BorderRadius.circular(13),
+                              border: Border.all(
+                                  color: Color(0xffb1aeae), width: 1)),
                           child: TextButton(
                             child: Text(
                               '취소',
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 12),
+                              style: TextStyle(
+                                  color: Color(0xff000000), fontSize: 12),
                             ),
                             style: ButtonStyle(
                               padding: MaterialStateProperty.all<EdgeInsets>(
                                   EdgeInsets.only(bottom: 1)),
                               tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                              // overlayColor: MaterialStateColor.resolveWith(
-                              //         (states) => Color(0xff0473e1)),
-                              // shape:
-                              // MaterialStateProperty.all<RoundedRectangleBorder>(
-                              //   RoundedRectangleBorder(
-                              //     borderRadius: BorderRadius.circular(13.sp),
-                              //   ),
-                              // ),
+                              overlayColor: MaterialStateColor.resolveWith(
+                                  (states) => Color(0xffe7e7e7)),
+                              shape: MaterialStateProperty.all<
+                                  RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(13),
+                                ),
+                              ),
                             ),
                             onPressed: () {
                               scenario.userActionSequence.add(userCancelAction);
 
                               Navigator.pop(context);
+                              if (aid == 'A1-b') {
+                                showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return CustomDialog(
+                                        sid: scenario.sid,
+                                        aid: 'A2-d',
+                                        height: 144,
+                                        userOkAction: U3_d,
+                                        userCancelAction: U3_e,
+                                      );
+                                    });
+                              } else if (aid == 'A2-d') {
+                                Get.off(A3cPage(
+                                  sid: sid,
+                                ));
+                              }
                             },
                           ),
                         ),
@@ -138,26 +218,27 @@ class CustomDialog extends StatelessWidget {
                           width: 89,
                           height: 26,
                           decoration: BoxDecoration(
-                              color: Color(0xff0473e1),
-                              borderRadius: BorderRadius.circular(13)),
+                            color: Color(0xff0473e1),
+                            borderRadius: BorderRadius.circular(13),
+                          ),
                           child: TextButton(
                             child: Text(
                               title,
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 12),
+                              style: TextStyle(
+                                  color: Color(0xffffffff), fontSize: 12),
                             ),
                             style: ButtonStyle(
                               padding: MaterialStateProperty.all<EdgeInsets>(
                                   EdgeInsets.only(bottom: 1)),
                               tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                              // overlayColor: MaterialStateColor.resolveWith(
-                              //         (states) => Color(0xff0473e1)),
-                              // shape:
-                              // MaterialStateProperty.all<RoundedRectangleBorder>(
-                              //   RoundedRectangleBorder(
-                              //     borderRadius: BorderRadius.circular(13.sp),
-                              //   ),
-                              // ),
+                              overlayColor: MaterialStateColor.resolveWith(
+                                  (states) => Color(0xff052b50)),
+                              shape: MaterialStateProperty.all<
+                                  RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(13),
+                                ),
+                              ),
                             ),
                             onPressed: () {
                               scenario.userActionSequence.add(userOkAction);
@@ -170,13 +251,13 @@ class CustomDialog extends StatelessWidget {
                                       return CustomDialog(
                                         sid: scenario.sid,
                                         aid: 'A2-c',
+                                        height: 168,
                                         userOkAction: U3_d,
                                         userCancelAction: U3_e,
                                       );
                                     });
-                              }
-                              else if (aid == 'A2-c') {
-                                Get.to(A3bPage());
+                              } else {
+                                Get.to(A3bPage(sid: scenario.sid));
                               }
                             },
                           ),
